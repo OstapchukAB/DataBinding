76232c92d6308ce95a7d7852afb8197540edc572
OstapchukAB <electron.anton@gmail.com>
2022-10-08 12:44:38 +0300

    Add project files.

---
+++ WFdataBinding/CustomerControl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFdataBinding
+{
+    // This class implements a simple user control
+    // that demonstrates how to apply the propertyNameChanged pattern.
+    [ComplexBindingProperties("DataSource", "DataMember")]
+    public class CustomerControl : UserControl
+    {
+        private DataGridView dataGridView1;
+        private Label label1;
+        private DateTime lastUpdate = DateTime.Now;
+
+        public EventHandler DataSourceChanged;
+
+        public object DataSource
+        {
+            get
+            {
+                return this.dataGridView1.DataSource;
+            }
+            set
+            {
+                if (DataSource != value)
+                {
+                    this.dataGridView1.DataSource = value;
+                    OnDataSourceChanged();
+                }
+            }
+        }
+
+        public string DataMember
+        {
+            get { return this.dataGridView1.DataMember; }
+
+            set { this.dataGridView1.DataMember = value; }
+        }
+
+        private void OnDataSourceChanged()
+        {
+            if (DataSourceChanged != null)
+            {
+                DataSourceChanged(this, new EventArgs());
+            }
+        }
+
+        public CustomerControl()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1.ColumnHeadersHeightSizeMode =
+               System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.dataGridView1.Location = new System.Drawing.Point(100, 100);
+            this.dataGridView1.Size = new System.Drawing.Size(500, 500);
+
+            this.dataGridView1.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer List:";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Size = new System.Drawing.Size(450, 250);
+        }
+    }
+}
---
+++ WFdataBinding/Form3.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+// Change the namespace to the project name.
+namespace WFdataBinding
+{
+    // This form demonstrates using a BindingSource to bind
+    // a list to a DataGridView control. The list does not
+    // raise change notifications. However the DemoCustomer1 type
+    // in the list does.
+    public partial class Form3 : Form
+    {
+        // This button causes the value of a list element to be changed.
+        private Button changeItemBtn = new Button();
+
+        // This DataGridView control displays the contents of the list.
+        private DataGridView customersDataGridView = new DataGridView();
+
+        // This BindingSource binds the list to the DataGridView control.
+        private BindingSource customersBindingSource = new BindingSource();
+
+        public Form3()
+        {
+            InitializeComponent();
+
+            // Set up the "Change Item" button.
+            this.changeItemBtn.Text = "Change Item";
+            this.changeItemBtn.Dock = DockStyle.Bottom;
+            this.changeItemBtn.Height = 100;
+            this.changeItemBtn.Click +=
+             new EventHandler(changeItemBtn_Click);
+            //this.changeItemBtn.Click +=
+            //changeItemBtn_Click;
+
+
+            this.Controls.Add(this.changeItemBtn);
+
+            // Set up the DataGridView.
+            customersDataGridView.Dock = DockStyle.Top;
+            this.Controls.Add(customersDataGridView);
+
+            this.Size = new Size(400, 200);
+            this.Load += Form3_Load; 
+        }
+
+        
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            this.Top = 100;
+            this.Left = 100;
+            this.Height = 600;
+            this.Width = 1000;
+
+            // Create and populate the list of DemoCustomer objects
+            // which will supply data to the DataGridView.
+            BindingList<DemoCustomer1> customerList = new();
+            customerList.Add(DemoCustomer1.CreateNewCustomer());
+            customerList.Add(DemoCustomer1.CreateNewCustomer());
+            customerList.Add(DemoCustomer1.CreateNewCustomer());
+
+            // Bind the list to the BindingSource.
+            this.customersBindingSource.DataSource = customerList;
+
+            // Attach the BindingSource to the DataGridView.
+            this.customersDataGridView.DataSource =
+                this.customersBindingSource;
+        }
+
+        // Change the value of the CompanyName property for the first
+        // item in the list when the "Change Item" button is clicked.
+        void changeItemBtn_Click(object sender, EventArgs e)
+        {
+            // Get a reference to the list from the BindingSource.
+            BindingList<DemoCustomer1>? customerList =
+                this.customersBindingSource.DataSource as BindingList<DemoCustomer1>;
+
+            // Change the value of the CompanyName property for the
+            // first item in the list.
+            if (customerList == null )
+                return;
+            customerList[0].CustomerName = "Tailspin Toys";
+            customerList[0].PhoneNumber = "(708)555-0150";
+        }
+
+    }
+
+    
+}
